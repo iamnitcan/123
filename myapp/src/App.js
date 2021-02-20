e7870559818012e355495f0f3a441390c5d08b9e
iamnitcan <iamnitcan@gmail.com>
2021-02-20 12:59:24 +0530

    2

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello LEARN REACT
+          Hello LEARN REAC
         </p>
         <a
           className="App-link"
